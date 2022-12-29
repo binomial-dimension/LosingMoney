3b7d4f0d77f877dece17c634ddcac89351bc313a
whispering-cloud <baobao_wu1@sina.com>
2022-12-29 14:19:45 +0800

    Create integer trading

--- trading/wy_baseline.py
+++ trading/wy_baseline.py
@@ -38,3 +38,42 @@ def wy_tradegy(predmax, predmin, truemax, truemin, trueopen, trueover, setwater 
     res += trueover*(ticket - setwater)
     ticket = setwater
     return res/trueover + setwater
+
+   
+@njit(fastmath=True)
+def wy_tradegy_int(predmax, predmin, truemax, truemin, trueopen, trueover, setwater = 0.5, maxm = 100000):
+    open_money = trueopen * (1 - setwater)
+    ticket = setwater
+    res = open_money
+    mid = trueopen
+    if truemin < mid:
+        theory_ke = int((mid - predmin) / 0.01)
+        real_ke = int((mid - truemin) / 0.01)
+        ssum = 0.0
+        for i in range(theory_ke):
+            ssum += selc_func(i)
+        
+        for i in range(min(real_ke, theory_ke)):
+            tmp = int((selc_func(i) / ssum) * setwater * maxm) / maxm
+            ticket += tmp
+            res -= (mid - i * 0.01) * tmp
+
+
+    if truemax > mid:
+        theory_ke = int((predmax - mid) / 0.01)
+        real_ke = int((truemax - mid) / 0.01)
+        ssum = 0.0
+        for i in range(theory_ke):
+            ssum += selc_func(i)
+        
+        for i in range(min(real_ke, theory_ke)):
+            tmp = int((selc_func(i) / ssum) * setwater * maxm) / maxm
+            ticket -= tmp
+            res += (mid + i * 0.01) * tmp
+            
+
+    res += trueover*(ticket - setwater)
+    ticket = setwater
+    return res/trueover + setwater
+
+     
