3edfd3b146da034d3505dc2669946c1d390c7a3f
frinkleko <frinkleko@gmail.com>
2022-12-29 10:15:18 +0800

    use todo list and add comments in config.py

--- model/config.py
+++ model/config.py
@@ -1,73 +1,100 @@
 import os
 import time
-
+from dataclasses import dataclass
 frame = "pytorch"
 
+@dataclass
 class Config:
-    # 数据参数
-    feature_columns = list(range(1, 40))     # 要作为feature的列，按原数据从0开始计算，也可以用list 如 [2,4,6,8] 设置
-    label_columns = [1,2,3,4]                  # 要预测的列，按原数据从0开始计算, 如同时预测第四，五列 最低价和最高价
-    # label_in_feature_index = [feature_columns.index(i) for i in label_columns]  # 这样写不行
-    label_in_feature_index = (lambda x,y: [x.index(i) for i in y])(feature_columns, label_columns)  # 因为feature不一定从0开始
+    """a datacalss to store all the config
+    """
+    # decide what features to use and what to predict    
+    # columns begin from 0
+    feature_columns = list(range(1, 40))
+    label_columns = [1,2,3,4]
 
-    predict_day = 1             # 预测未来几天
+    # get label index in feature
+    label_in_feature_index = (lambda x,y: [x.index(i) for i in y])(feature_columns, label_columns)
 
-    # 网络参数
+    # how many days to predict
+    predict_day = 1
+
+    # net parameters
     input_size = len(feature_columns)
     output_size = len(label_columns)
 
-    hidden_size = 200           # LSTM的隐藏层大小，也是输出大小
-    lstm_layers =  2            # LSTM的堆叠层数
-    dropout_rate = 0.2          # dropout概率
-    time_step = 40              # 这个参数很重要，是设置用前多少天的数据来预测，也是LSTM的time step数，请保证训练数据量大于它
+    # LSTM hidden size
+    hidden_size = 200
+    # LSTM layers
+    lstm_layers =  2
+    # dropout rate
+    dropout_rate = 0.2
+    # time step use last n days to predict
+    time_step = 40
 
-    # 训练参数
+    # train setting
     do_train = True
     do_predict = True
-    add_train = False          # 是否载入已有模型参数进行增量训练
-    shuffle_train_data = True   # 是否对训练数据做shuffle
-    use_cuda = True           # 是否使用GPU训练
 
-    train_data_rate = 0.83      # 训练数据占总体数据比例，测试数据就是 1-train_data_rate
-    valid_data_rate = 0.05      # 验证数据占训练数据比例，验证集在训练过程使用，为了做模型和参数选择
+    # if do continue train
+    add_train = False
+    # if shuffle train data
+    shuffle_train_data = True
+    # if use cuda, is gpu available and cuda is installed
+    use_cuda = True
+
+    # train data rate, test data is 1-train_data_rate
+    train_data_rate = 0.83
+    # valid data rate, valid data is train_data_rate*valid_data_rate
+    valid_data_rate = 0.05
 
     batch_size = 1024
     learning_rate = 0.001
-    epoch = 1000                  # 整个训练集被训练多少遍，不考虑早停的前提下
-    patience = 40               # 训练多少epoch，验证集没提升就停掉
-    random_seed = 42            # 随机种子，保证可复现
+    epoch = 1000
+    # early stop patience
+    patience = 40
+    random_seed = 42
 
-    do_continue_train = False    # 每次训练把上一次的final_state作为下一次的init_state，仅用于RNN类型模型，目前仅支持pytorch
-    continue_flag = ""           # 但实际效果不佳，可能原因：仅能以 batch_size = 1 训练
+    # use last state as the initial state of the next batch
+    do_continue_train = False
+    continue_flag = ""
 
+    # continue train setting
     if do_continue_train:
         shuffle_train_data = False
         batch_size = 1
         continue_flag = "continue_"
 
-    # 训练模式
-    debug_mode = False # 调试模式下，是为了跑通代码，追求快
-    debug_num = 500  # 仅用debug_num条数据来调试
+    # debug setting
+    # to debug, set debug_mode to True
+    debug_mode = False
+    # in debug mode, use debug_num data to train
+    debug_num = 500
 
-    # 框架参数
-    used_frame = frame  # 选择的深度学习框架，不同的框架模型保存后缀不一样
+    # frame setting
+    used_frame = frame
     model_postfix = {"pytorch": ".pth", "keras": ".h5", "tensorflow": ".ckpt"}
     model_name = "model_" + continue_flag + used_frame + model_postfix[used_frame]
 
-    # 路径参数
+    # path setting
     train_data_path = "../data/processed.csv"
     model_save_path = "./checkpoint/" + used_frame + "/"
     figure_save_path = "./figure/"
     log_save_path = "./log/"
+    # log setting
     do_log_print_to_screen = True
-    do_log_save_to_file = True                  # 是否将config和训练过程记录到log
+    do_log_save_to_file = True
     do_figure_save = False
-    do_train_visualized = False          # 训练loss可视化，pytorch用visdom，tf用tensorboardX，实际上可以通用, keras没有
+    # if do train loss visualization with visdom
+    do_train_visualized = False
 
+    # path generate
+    # model save path
     if not os.path.exists(model_save_path):
-        os.makedirs(model_save_path)    # makedirs 递归创建目录
+        os.makedirs(model_save_path)
+    # figure save path
     if not os.path.exists(figure_save_path):
         os.mkdir(figure_save_path)
+    # log save path
     if do_train and (do_log_save_to_file or do_train_visualized):
         cur_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
         log_save_path = log_save_path + cur_time + '_' + used_frame + "/"
