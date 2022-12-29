a2c0607a34524554347f1f6119679cb0cdc4de43
frinkleko <frinkleko@gmail.com>
2022-12-29 10:42:42 +0800

    add comments fir lstm_solution.py

--- model/lstm_solution.py
+++ model/lstm_solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
 from config import Config
 from data import Data
@@ -17,50 +16,67 @@ def draw(config: Config, origin_data: Data, logger, predict_norm_data: np.ndarra
         config (Config): store all the config
         origin_data (Data): a class store the origin and processed data
         logger (Logger): a logger
-        predict_norm_data (np.ndarray): the normalized predicted data
+        predict_norm_data (numpy array): the normalized predicted data
 
     Returns:
         four predict and label data: lose_truth,close,open_truth,open,high_truth,high,low_truth,low
     """    
     label_data = origin_data.data[origin_data.train_num + origin_data.start_num_in_test : ,
                                             config.label_in_feature_index]
+    # denormalize the data
     predict_data = predict_norm_data * origin_data.std[config.label_in_feature_index] + \
-                   origin_data.mean[config.label_in_feature_index]   # 通过保存的均值和方差还原数据
+                   origin_data.mean[config.label_in_feature_index]
+    
+    # check the element number in origin and predicted data
     assert label_data.shape[0]==predict_data.shape[0], "The element number in origin and predicted data is different"
 
     label_name = [origin_data.data_column_name[i] for i in config.label_in_feature_index]
     label_column_num = len(config.label_columns)
 
+    # calculate the mean squared error
     loss = np.mean((label_data[config.predict_day:] - predict_data[:-config.predict_day] ) ** 2, axis=0)
     loss_norm = loss/(origin_data.std[config.label_in_feature_index] ** 2)
     logger.info("The mean squared error of stock {} is ".format(label_name) + str(loss_norm))
 
+    # draw the predict and label data
+
     label_X = range(origin_data.data_num - origin_data.train_num - origin_data.start_num_in_test)
     predict_X = [ x + config.predict_day for x in label_X]
 
-    if not sys.platform.startswith('linux'):    # 无桌面的Linux下无法输出，如果是有桌面的Linux，如Ubuntu，可去掉这一行
-        for i in range(label_column_num):
-            plt.figure(i+1)                     # 预测数据绘制
-            plt.plot(label_X, label_data[:, i], label='label')
-            plt.plot(predict_X, predict_data[:, i], label='predict')
-            plt.title("Predict stock {} price with {}".format(label_name[i], config.used_frame))
-            mape = np.mean(np.abs((label_data[config.predict_day:, i] - predict_data[:-config.predict_day, i]) / label_data[config.predict_day:, i]))
-            plt.text(0.5, 0.5, "The mean squared percentage error is {}".format(mape), fontsize=10)
-            logger.info("The mean squared percentage error of stock {} is ".format(label_name[i]) + str(mape))
-            print("The mean squared percentage error of stock {} is ".format(label_name[i]) + str(mape))
-            logger.info("The predicted stock {} for the next {} day(s) is: ".format(label_name[i], config.predict_day) +
-                  str(np.squeeze(predict_data[-config.predict_day:, i])))
-            if config.do_figure_save:
-                plt.savefig(config.figure_save_path+"{}predict_{}_with_{}.png".format(config.continue_flag, label_name[i], config.used_frame))
+    for i in range(label_column_num):
+        plt.figure(i+1)
+        plt.plot(label_X, label_data[:, i], label='label')
+        plt.plot(predict_X, predict_data[:, i], label='predict')
+        plt.title("Predict stock {} price with {}".format(label_name[i], config.used_frame))
+        plt.xlabel("Time")
+        plt.ylabel("Price")
+        plt.legend()
 
-        plt.show()
+        # calculate the mean squared percentage error
+        mape = np.mean(np.abs((label_data[config.predict_day:, i] - predict_data[:-config.predict_day, i]) / label_data[config.predict_day:, i]))
+        logger.info("The mean squared percentage error of stock {} is ".format(label_name[i]) + str(mape))
+        print("The mean squared percentage error of stock {} is ".format(label_name[i]) + str(mape))
+
+        logger.info("The predicted stock {} for the next {} day(s) is: ".format(label_name[i], config.predict_day) +
+                str(np.squeeze(predict_data[-config.predict_day:, i])))
+        
+        # save the figure
+        if config.do_figure_save:
+            plt.savefig(config.figure_save_path+"{}predict_{}_with_{}.png".format(config.continue_flag, label_name[i], config.used_frame))
+
+    plt.show()
     return label_data[:, 0], predict_data[:, 0],label_data[:, 1], predict_data[:, 1],label_data[:, 2], predict_data[:, 2],label_data[:,3],predict_data[:,3]
 
 
 def main(config):
+    """main function
+
+    Args:
+        config (Config): store all the config
+    """    
     logger = load_logger(config)
     try:
-        np.random.seed(config.random_seed)  # 设置随机种子，保证可复现
+        np.random.seed(config.random_seed)
         data_gainer = Data(config)
 
         if config.do_train:
@@ -69,9 +85,12 @@ def main(config):
 
         if config.do_predict:
             test_X, test_Y = data_gainer.get_test_data(return_label_data=True)
-            pred_result = predict(config, test_X)       # 这里输出的是未还原的归一化预测数据
+
+            # denormalized predict data
+            pred_result = predict(config, test_X) 
             close_truth,close,open_truth,open,high_truth,high,low_truth,low = draw(config, data_gainer, logger, pred_result)
 
+            # output the predict data to csv file
             output = pd.DataFrame({'close':close,'close_truth':close_truth,'open':open,'open_truth':open_truth,'high':high,'high_truth':high_truth,'low':low,'low_truth':low_truth})
             output.to_csv('../data/predict.csv', index=False)
     except Exception:
