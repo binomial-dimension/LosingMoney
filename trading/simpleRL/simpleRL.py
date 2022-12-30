d89393b1da8bc61d3624b6df20fe8511b50be7d1
frinkleko <frinkleko@gmail.com>
2022-12-30 11:52:20 +0800

    sort out file

--- trading/simpleRL/simpleRL.py
+++ trading/simpleRL/simpleRL.py
@@ -4,7 +4,9 @@ import pandas as pd
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
-from env import StockTradingEnv
+import gym
+from gym import spaces
+# from env import StockTradingEnv
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,21 +15,172 @@ sns.set()
 
 np.random.seed(0)
 
-def stock_trade(stock_file):
+MAX_ACCOUNT_BALANCE = 2147483647
+MAX_NUM_SHARES = 2147483647
+MAX_SHARE_PRICE = 6000
+MAX_VOLUME = 1000
+MAX_OPEN_POSITIONS = 5
+MAX_STEPS = 20000
+MAX_DAY_CHANGE = 1
 
+INITIAL_ACCOUNT_BALANCE = 100000
+
+
+class StockTradingEnv(gym.Env):
+    """A stock trading environment for OpenAI gym"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, df):
+        super(StockTradingEnv, self).__init__()
+
+        self.df = df
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+
+        # Actions of the format Buy x%, Sell x%, Hold, etc.
+        self.action_space = spaces.Box(
+            low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
+
+        # Prices contains the OHCL values for the last five prices
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=(4*41 + 6,), dtype=np.float16)
+        self.last_net_worth = INITIAL_ACCOUNT_BALANCE
+
+    def _next_observation(self):
+        obs1 = np.array([
+            self.df.loc[self.current_step - 40:self.current_step, 'open_truth'] + self.df.loc[self.current_step, 'open'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step - 40:self.current_step, 'high_truth'] + self.df.loc[self.current_step, 'high'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step - 40:self.current_step, 'low_truth'] + self.df.loc[self.current_step, 'low'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step - 40:self.current_step, 'close_truth'] + self.df.loc[self.current_step, 'close'] / MAX_SHARE_PRICE,
+        ])
+        obs1 = obs1.flatten()
+        obs2 = np.array([
+            self.balance / MAX_ACCOUNT_BALANCE,
+            self.max_net_worth / MAX_ACCOUNT_BALANCE,
+            self.shares_held / MAX_NUM_SHARES,
+            self.cost_basis / MAX_SHARE_PRICE,
+            self.total_shares_sold / MAX_NUM_SHARES,
+            self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
+        ])
+        obs = np.concatenate((obs1, obs2), axis=0)
+        return obs
+
+    def _take_action(self, action):
+        # Set the current price to a random price within the time step
+        current_price = np.random.uniform(
+            self.df.loc[self.current_step, "open_truth"], self.df.loc[self.current_step, "close_truth"])
+
+        action_type = action[0]
+        amount = action[1]
+        if int(int(self.balance / current_price) * amount) == 0:
+            action_type = 2
+
+        if action_type < 1:
+            # Buy amount % of balance in shares
+
+            # the max possible number that can be bought
+            total_possible = int(self.balance / current_price)
+
+            # the number that will be bought
+            shares_bought = int(total_possible * amount)
+
+            # avoid buying price
+            prev_cost = self.cost_basis * self.shares_held
+            additional_cost = shares_bought * current_price
+
+            fee = shares_bought/10000*1.1
+            additional_cost += fee
+
+            self.balance -= additional_cost
+
+            # update cost basis
+            self.cost_basis = (prev_cost + additional_cost) / (self.shares_held + shares_bought)
+            
+            # how many shares are held
+            self.shares_held += shares_bought
+
+        elif action_type < 2:
+            # Sell amount % of shares held
+            shares_sold = int(self.shares_held * amount)
+
+            fee = shares_sold/10000*1.1
+            shares_sold -= fee
+            
+            self.balance += shares_sold * current_price
+            self.shares_held -= shares_sold
+            self.total_shares_sold += shares_sold
+            self.total_sales_value += shares_sold * current_price
+
+        self.last_net_worth = self.net_worth
+        self.net_worth = self.balance + self.shares_held * self.df.loc[self.current_step, "close_truth"]
+
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = self.net_worth
+
+        if self.shares_held == 0:
+            self.cost_basis = 0
+
+    def step(self, action):
+        # Execute one time step within the environment
+        self._take_action(action)
+        done = False
+        
+
+        self.current_step += 1
+
+        if self.current_step > len(self.df.loc[:, 'open_truth'].values) - 1:
+            self.current_step = 41  # loop training
+        # profits
+        reward = 0.2*self.net_worth/self.last_net_worth + 0.8*self.net_worth / INITIAL_ACCOUNT_BALANCE - 1
+
+        if self.net_worth <= 0:
+            done = True
+
+        obs = self._next_observation()
+
+        return obs, reward, done, {}
+
+    def reset(self, new_df=None):
+        # Reset the state of the environment to an initial state
+        self.balance = INITIAL_ACCOUNT_BALANCE
+        self.net_worth = INITIAL_ACCOUNT_BALANCE
+        self.max_net_worth = INITIAL_ACCOUNT_BALANCE
+        self.shares_held = 0
+        self.cost_basis = 0
+        self.total_shares_sold = 0
+        self.total_sales_value = 0
+
+        # pass test dataset to environment
+        if new_df:
+            self.df = new_df
+
+        # set current step to 0 
+        self.current_step = 41
+
+        return self._next_observation()
+
+    def render(self, mode='human', close=False):
+        # Render the environment to the screen
+        profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        
+        return self.balance, self.net_worth, profit
+
+
+def stock_trade(stock_file)
     df = pd.read_csv(stock_file)
 
     # The algorithms require a vectorized environment to run
     env = DummyVecEnv([lambda: StockTradingEnv(df)])
-    if os.path.exists('./model/stock_trade'):
-        model = PPO2.load('./model/stock_trade',env=env)
-    else:
-        model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='./log')
-    
-    model.learn(total_timesteps=int(1e7),log_interval=1e3)
     try:
-        os.remove('./model/stock_trade')
+        model = PPO2.load('./model/stock_trade',env=env)
+        print('load model')
+    except:
+        model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='./log') #,gamma=0.99,learning_rate=0.00025,n_steps=2048,ent_coef=0.01,cliprange=0.2,cliprange_vf=-1,nminibatches=4,noptepochs=10,lam=0.95,verbose=1)
+        print('new model')
+    
+    model.learn(total_timesteps=int(5e4),log_interval=1e3)
+    try:
         model.save('./model/stock_trade')
+        print('save model')
     except:
         print('model already exists')
 
@@ -80,9 +233,9 @@ def test_a_stock_trade(stock_code='hs300'):
     close_truth = np.array(df_test.close_truth)
     close = np.array(df_test.close)
 
-    ax.plot(close_truth/begin, label='truth')
     ax.plot(close_truth/begin,marker='^',label='buy',markevery=buy,markersize=5)
     ax.plot(close_truth/begin,marker='v',label='sell',markevery=sell,markersize=5)
+    ax.plot(close_truth/begin, label='truth')
 
     #ax.plot(blances/blances[0], label='blance')
     ax.plot(net_worths/net_worths[0], label='net_worth')
